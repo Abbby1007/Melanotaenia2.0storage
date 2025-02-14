ec24265b5cd1fb70c141b4df954b6efe22293a97
Abbby1007 <Abigailloken25@gmail.com>
2025-02-14 16:45:15 +0900

    Finished up tutorial

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -80,5 +80,5 @@ func _on_area_2d_area_entered(area):
 	#------------------------------------------------------
 	
 	if area.get_parent().is_in_group("Tutorial_Done"):
-		label.text = "DONEEEE"
+		label.text = "🏁🏁🏁🏁🏁🏁🏁🏁"
 		
